7122e5f2528bc67216a661079a9042e0f2b587eb
tomas-ryan-lee <contact@creative-eye.fr>
2025-02-02 23:07:56 +0100

    Structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PricingModule } from './pricing/pricing.module';
 
 @Module({
-  imports: [],
+  imports: [PricingModule],
   controllers: [AppController],
   providers: [AppService],
 })
